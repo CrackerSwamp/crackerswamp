de80118965d5912d2dc6d16b462b14dcaae0d438
CrackerSwamp <ronaldjoiner1@gmail.com>
2026-04-15 17:09:03 -0400

    data.js

---
+++ data/data.js
@@ -0,0 +1,66 @@
+// CRACKER SWAMP DATA SYSTEM
+
+export const bands = [
+  {
+    id: "tunabunny",
+    name: "Tunabunny",
+    location: "Athens, GA",
+    genre: "Indie / Punk",
+    image: "/assets/bands/tunabunny.jpg",
+    links: {
+      instagram: "",
+      spotify: "",
+      youtube: ""
+    }
+  },
+  {
+    id: "nuclear-tourist",
+    name: "Nuclear Tourist",
+    location: "Atlanta, GA",
+    genre: "Punk",
+    image: "/assets/bands/nuclear-tourist.jpg",
+    links: {}
+  },
+  {
+    id: "the-woggles",
+    name: "The Woggles",
+    location: "Atlanta, GA",
+    genre: "Garage Rock",
+    image: "/assets/bands/woggles.jpg",
+    links: {}
+  }
+];
+
+export const videos = [
+  {
+    id: "vid1",
+    bandId: "tunabunny",
+    title: "Tunabunny - Live",
+    youtubeId: "dQw4w9WgXcQ",
+    state: "georgia"
+  },
+  {
+    id: "vid2",
+    bandId: "nuclear-tourist",
+    title: "Nuclear Tourist - Track",
+    youtubeId: "ysz5S6PUM-U",
+    state: "georgia"
+  }
+];
+
+export const radio = [
+  {
+    id: "track1",
+    title: "Swamp Static",
+    band: "Tunabunny",
+    file: "/assets/music/swamp-static.mp3",
+    category: "Georgia"
+  },
+  {
+    id: "track2",
+    title: "Backwoods Signal",
+    band: "Nuclear Tourist",
+    file: "/assets/music/backwoods-signal.mp3",
+    category: "Georgia"
+  }
+];

6a2bf6c3c24925a6695cf475dc54bdb3d688a651
CrackerSwamp <ronaldjoiner1@gmail.com>
2026-04-16 21:21:35 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const bands = [
+
+  {
+    name: "Band One",
+    location: "Atlanta, GA",
+    video: "https://www.youtube.com/watch?v=XXXX",
+    mp3: "song1.mp3"
+  },
+
+  {
+    name: "Band Two",
+    location: "Savannah, GA",
+    video: "https://www.youtube.com/watch?v=YYYY",
+    mp3: "song2.mp3"
+  }
+
+];
